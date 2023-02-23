b99972548696bd23c743e3beb4c56bed921402e5
dtidwell <conversations.and.L@gmail.com>
2023-02-23 17:10:57 -0600

    add functions

--- app/src/test/java/aipr/JSON_resultTest.java
+++ app/src/test/java/aipr/JSON_resultTest.java
@@ -1,7 +1,12 @@
 package aipr;
 
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class JSON_resultTest {
 
+    @Test
+    public void saveMapToJson() {
+    }
 }
--- app/src/test/java/aipr/OpenAiServeTest.java
+++ app/src/test/java/aipr/OpenAiServeTest.java
@@ -1,7 +1,20 @@
 package aipr;
 
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class OpenAiServeTest {
 
+    @Test
+    public void makeRequest() {
+    }
+
+    @Test
+    public void addToMap() {
+    }
+
+    @Test
+    public void getResultsMap() {
+    }
 }
